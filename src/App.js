c502c732e8dd7c547505c925f008dd131d13a018
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-11-12 12:46:58 +0200

    add categories buttons logic

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState([]);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
 
   return (
     <section className='menu section'>
@@ -13,7 +25,7 @@ function App() {
         <h2>our menu</h2>
         <div className='underline'></div>
       </div>
-      <Categories />
+      <Categories filterItems={filterItems} categories={allCategories} />
       <Menu items={menuItems} />
     </section>
   );
